5575b60181dae463742ff7baa13b666a98162125
Nikhil Navadiya <nikhil@awaaz.de>
2017-02-28 20:04:21 +0530

    removed dist

--- dist/busy.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { BusyConfig } from './busy-config';
-export declare class BusyModule {
-    static forRoot(config: BusyConfig): ModuleWithProviders;
-}
